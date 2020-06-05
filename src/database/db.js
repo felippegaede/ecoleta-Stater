56e4f1777dba3fc44040e48e44ec67b7285dfb04
Felippe Gaede <felippe.gaede@hotmail.com>
2020-06-05 18:47:50 -0300

    Finalização do projeto

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,54 @@
 const sqlite3 = require("sqlite3").verbose()
-const db = new sqlite3.Database("./src/datatbase/database.db")
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
+// db.serialize(() => {
+//     db.run(`
+//         create table if not exists places (
+//             id integer primary key autoincrement,
+//             name text,
+//             image text,
+//             address text,
+//             address_number text,
+//             state text,
+//             city text,
+//             items text
+//         );
+//     `)
+
+//     const query = `insert into places (image, name, address, address_number, state, city, items) values (?,?,?,?,?,?,?);`
+//     const values = [
+//         "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+//         "Papersider",
+//         "Guilherme Gemballa, Jardim América",
+//         "260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Papéis e Papelão"
+//     ]
+
+//     function afterInsertData(err){
+
+//         if (err){
+//             return console.log(err)
+//         }
+
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+
+//     }
+
+    // db.all(`delete from places`, function(err, rows){
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     console.log("Aqui estão seus registros")
+    //     console.log(rows)
+    // })
+
+//     db.run(query, values, afterInsertData())
+
+
+// })
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db")
+
 server.use(express.static("public"))
+
+server.use(express.urlencoded({extended: true}))
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -21,10 +25,57 @@ server.get("/create-points", (req, res) => {
 
 })
 
+server.post("/create-points", (req, res) => {
+
+    const query = `insert into places (image, name, address, address_number, state, city, items) values (?,?,?,?,?,?,?);`
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address_number,
+        req.body.stateName,
+        req.body.city,
+        req.body.items,
+ 
+    ]
+
+        function afterInsertData(err){
+
+        if (err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-points.html",{saved: true})
+
+    }
+
+    db.run(query, values, afterInsertData())
+    
+    
+
+})
+
 server.get("/search-results", (req, res) => {
 
-    return res.render("search-results.html")
+    const search = req.query.search
 
+    if(search == ""){
+
+        return res.render("search-results.html", {totals: 0})
+    }
+
+    db.all(`Select * from places where city like '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const totals = rows.length
+
+        return res.render("search-results.html", {places: rows, totals: totals})
+    })
 })
 
 server.listen(3000)
